91d8c33dd7111091497513728e3d58ac7200c200
Taf Makura <tafmakura@gmail.com>
2025-05-30 23:39:03 +0200

    update

--- includes/classes/core/class-plugin.php
+++ includes/classes/core/class-plugin.php
@@ -8,73 +8,34 @@ if (!defined('ABSPATH')) {
 
 /**
  * Main Plugin Class
- *
- * @package ArsolPluginBoilerplate\Classes\Core
  */
 class Plugin {
     /**
      * Plugin instance
-     *
      * @var Plugin
      */
     private static $instance = null;
 
     /**
-     * Admin instance
-     *
-     * @var \ArsolPluginBoilerplate\Classes\Admin\Admin
-     */
-    private $admin;
-
-    /**
-     * Shortcodes instance
-     * @var \ArsolPluginBoilerplate\Classes\Frontend\Shortcodes
-     */
-    private $shortcodes;
-
-    /**
-     * Assets instance
-     * @var \ArsolPluginBoilerplate\Classes\Core\Assets
-     */
-    private $assets;
-
-    /**
      * Constructor
      */
     private function __construct() {
-        $this->define_constants();
         $this->init_hooks();
-    }
-
-    /**
-     * Define plugin constants
-     */
-    private function define_constants() {
-        if (!defined('ARSOL_PLUGIN_VERSION')) {
-            define('ARSOL_PLUGIN_VERSION', '1.0.0');
-        }
-        if (!defined('ARSOL_PLUGIN_PATH')) {
-            define('ARSOL_PLUGIN_PATH', plugin_dir_path(dirname(dirname(dirname(__FILE__)))));
-        }
-        if (!defined('ARSOL_PLUGIN_URL')) {
-            define('ARSOL_PLUGIN_URL', plugin_dir_url(dirname(dirname(dirname(__FILE__)))));
-        }
     }
 
     /**
      * Initialize hooks
      */
     private function init_hooks() {
-        // Initialize admin
-        if (is_admin()) {
-            $this->admin = new \ArsolPluginBoilerplate\Classes\Admin\Admin();
-        }
+        // Add your hooks here
+        add_action('init', array($this, 'init'));
+    }
 
-        // Initialize shortcodes
-        $this->shortcodes = new \ArsolPluginBoilerplate\Classes\Frontend\Shortcodes();
-
-        // Initialize assets
-        $this->assets = new \ArsolPluginBoilerplate\Classes\Core\Assets();
+    /**
+     * Initialize plugin
+     */
+    public function init() {
+        // Initialize plugin
     }
 
     /**
@@ -93,7 +54,6 @@ class Plugin {
 
     /**
      * Get plugin instance
-     *
      * @return Plugin
      */
     public static function get_instance() {
@@ -101,32 +61,5 @@ class Plugin {
             self::$instance = new self();
         }
         return self::$instance;
-    }
-
-    /**
-     * Get admin instance
-     *
-     * @return \ArsolPluginBoilerplate\Classes\Admin\Admin
-     */
-    public function get_admin() {
-        return $this->admin;
-    }
-
-    /**
-     * Get shortcodes instance
-     *
-     * @return \ArsolPluginBoilerplate\Classes\Frontend\Shortcodes
-     */
-    public function get_shortcodes() {
-        return $this->shortcodes;
-    }
-
-    /**
-     * Get assets instance
-     *
-     * @return \ArsolPluginBoilerplate\Classes\Core\Assets
-     */
-    public function get_assets() {
-        return $this->assets;
     }
 } 
