8705a4eca1f337a6da7f87f3d4a7ceb7b0475cef
Taf Makura <tafmakura@gmail.com>
2025-05-30 23:25:54 +0200

    Update class-autoloader.php

--- includes/classes/core/class-autoloader.php
+++ includes/classes/core/class-autoloader.php
@@ -36,12 +36,22 @@ class Autoloader {
         // Remove namespace from class name
         $class = str_replace('ArsolPluginBoilerplate\\', '', $class);
 
-        // Convert class name format to file name format
-        $class = strtolower(str_replace('\\', '-', $class));
-        $class = 'class-' . $class . '.php';
+        // Split the class name into parts
+        $parts = explode('\\', $class);
+        
+        // Convert the class name to file name format
+        $class_name = strtolower(str_replace('_', '-', end($parts)));
+        $class_name = 'class-' . $class_name . '.php';
 
-        // Build the file path
-        $file = ARSOL_PLUGIN_DIR . 'includes/classes/' . $class;
+        // Build the directory path
+        $dir_parts = array_slice($parts, 0, -1);
+        $dir_path = '';
+        if (!empty($dir_parts)) {
+            $dir_path = strtolower(implode('/', $dir_parts)) . '/';
+        }
+
+        // Build the full file path
+        $file = ARSOL_PLUGIN_DIR . 'includes/classes/' . $dir_path . $class_name;
 
         // If the file exists, require it
         if (file_exists($file)) {
